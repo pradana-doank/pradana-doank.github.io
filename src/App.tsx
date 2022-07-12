f308cf7f298fdea976391d3f8194d179a1bd14b8
pradana-doank <muhammaddanar@gmail.com>
2022-07-12 21:12:50 +0700

    refactor: update route entry

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+  )
+}
+
+export default App
