caa51d43248cfeb87b8c287d9ddfa2efb288188c
pradana-doank <muhammaddanar@gmail.com>
2022-07-12 21:10:43 +0700

    feat: added react icons and react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const container = document.getElementById('root')!
+const root = createRoot(container)
+
+root.render(
+  <ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ChakraProvider>
 )
