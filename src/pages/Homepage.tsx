44c39e0cc407e762ef4860c7fddace2c193d7791
pradana-doank <muhammaddanar@gmail.com>
2022-07-13 21:10:58 +0700

    refactor: update layout homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,12 +1,19 @@
 import { Container } from '@chakra-ui/react'
+import { useEffect } from 'react'
 import Appbar from '../components/Appbar'
 import Content from '../components/Content'
+import Footer from '../components/Footer'
 
 const Homepage = () => {
+  useEffect(() => {
+    document.title = 'Homepage'
+  }, [])
+
   return (
     <Container maxW="container.lg">
       <Appbar />
       <Content />
+      <Footer />
     </Container>
   )
 }
