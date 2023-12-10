9a1602c9631d799fe930b9f54f9b85b887143a17
danz4648 <danarputrapradana@yahoo.com>
2023-12-10 17:33:59 +0700

    update new website soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import { Container } from '@chakra-ui/react'
-import { Route, Routes } from 'react-router-dom'
-import Appbar from './components/Appbar'
-import Footer from './components/Footer'
-import Homepage from './pages/Homepage'
-import NotFound from './pages/NotFound'
-import Work from './pages/Work'
-
 import './index.css'
-import About from './pages/About'
 
 const App = () => {
   return (
     <Container maxW="container.lg">
-      <Appbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/projects" element={<Work />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
+      <h1>Coming Soon</h1>
+      <h1>Will be update soon</h1>
     </Container>
   )
 }
